ad3c504cb5281f4d8d643939d94411476eec0136
Developers@Work <DevelopersWork@users.noreply.github.com>
2024-03-24 23:58:34 +0530

    Assignment 1: Create the src_hosts model

---
+++ dbtlearn/models/src/src_hosts.sql
@@ -0,0 +1,12 @@
+WITH raw_hosts AS (
+    SELECT * from AIRBNB.RAW.RAW_HOSTS
+)
+
+SELECT 
+    id AS host_id,
+    name as host_name, 
+    is_superhost,
+    created_at,
+    updated_at
+FROM
+    raw_hosts
