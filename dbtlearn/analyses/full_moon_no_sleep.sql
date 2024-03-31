76a6b79b2b7cea5a4b3273813ba35b6165d8c82f
Developers@Work <DevelopersWork@users.noreply.github.com>
2024-03-31 10:05:20 +0530

    74. Hooks

---
+++ dbtlearn/analyses/full_moon_no_sleep.sql
@@ -0,0 +1,11 @@
+WITH mart_fullmoon_reviews AS (
+    SELECT * FROM {{ ref('mart_fullmoon_reviews') }}
+)
+
+SELECT
+    is_full_moon,
+    review_sentiment,
+    count(*) as reviews
+FROM mart_fullmoon_reviews
+GROUP BY 1, 2
+ORDER BY 1, 2
